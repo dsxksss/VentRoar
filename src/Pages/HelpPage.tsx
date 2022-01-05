9caf0ec18b9883972febcb04e8efe8799c255005
15270494073 <1637259043@qq.com>
2022-01-05 22:40:02 +0800

    增加了设置边框，中间背景

--- src/Pages/HelpPage.tsx
+++ src/Pages/HelpPage.tsx
@@ -3,11 +3,18 @@ import { AdjustmentsIcon } from "@heroicons/react/outline";
 function HelpPage() {
   return (
     <>
-      <div className="a">
-        <div className="beijing">
-          <AdjustmentsIcon className="h-10 w-10" /> help页面的一些内容
+      <div className="box">
+        <div className="szk">
+          <a href="">帮助</a>
+          <a href="">关于vent-roar</a>
+          <a href="">设置</a>
         </div>
       </div>
+        <div className="zjym" >
+          <div className="zjb">
+            <p className="zjz">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, sequi.</p>
+          </div>
+        </div>
     </>
   );
 }
