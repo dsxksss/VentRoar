ee36d52f94b2ac9d87308b11282b2f24ce0c9e2d
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-05 15:50:43 +0800

    图标库的使用

--- src/Pages/HelpPage.tsx
+++ src/Pages/HelpPage.tsx
@@ -1,12 +1,13 @@
 import "../css/helpPage.css";
-
+import { AdjustmentsIcon } from "@heroicons/react/outline";
 function HelpPage() {
   return (
     <>
       <div className="a">
-        <div className="beijing">help页面的一些内容</div>
+        <div className="beijing">
+          <AdjustmentsIcon className="h-10 w-10" /> help页面的一些内容
+        </div>
       </div>
-      
     </>
   );
 }
