b8bb600c7d9aa629ce71db95107ec65ea91b7b1d
15270494073 <1637259043@qq.com>
2022-01-08 20:15:38 +0800

    帮助页面修改

--- src/Pages/HelpPage.tsx
+++ src/Pages/HelpPage.tsx
@@ -4,15 +4,25 @@ function HelpPage() {
   return (
     <>
       <div className="box">
-        <div className="szk">
+        <div className="szk an-3">
+          <div className="an-5">
           <a href="">帮助</a>
           <a href="">关于vent-roar</a>
           <a href="">设置</a>
+          </div>
         </div>
       </div>
-        <div className="zjym" >
-          <div className="zjb">
-            <p className="zjz">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, sequi.</p>
+        <div className="zjym an-3">
+          <div className="zjb shadow-xl">
+            <p className="zjz an-5">Lorem ipsum dolor, sit amet 
+            consectetu radipisicing elit.Incidunt, Lorem ipsum dolor
+            sit amet Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Cupiditate dicta ducimus earum, 
+            dolor cumque libero maxime nulla sit quas 
+            facilis rem provident quia fugiat odit dolorem 
+            <b>nihil cum voluptates aliquid laudantium ab,</b> 
+            ratione soluta ex? Obcaecati inventore dignissimos amet quae 
+            veritatis provident ab nihil itaque, placeat quod a, non ut.</p>
           </div>
         </div>
     </>
