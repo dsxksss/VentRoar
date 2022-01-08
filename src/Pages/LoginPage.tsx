0b67b30ee08e9dc8c94b1f52bb42622fabaefacc
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-08 19:16:27 +0800

    fix登入页面bug

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -32,7 +32,7 @@ const Login = () => {
           {/* <p className="title-size">WelCome To</p> */}
           <p className="ventroar-size text-blue-500">VentRoar</p>
         </div>
-        <div id="input" className="space-y-6 lg:space-y-10">
+        <div id="input" className="flex flex-col space-y-6 lg:space-y-10">
           <div id="uesrText" className="loginPage-input">
             <UserCircleIcon className="w-5 h-5 mr-1" />
             <input
