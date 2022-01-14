2bca3a28d64c70f54440702a41328bd8a5c1de42
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-14 12:56:25 +0800

    fix:修改导航栏按钮定位

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -14,18 +14,20 @@ const Nav = () => {
   const { pathname } = useLocation();
   //FUNCTION:动态显示当前点击的网页
   const ShowActive = (pagaName: string) =>
-    pathname === pagaName ? "text-black" : "bt-style";
+    pathname === pagaName
+      ? "text-black border-b-2 border-slate-800 pb-2"
+      : "bt-style";
 
   return (
-    <div className="mx-2 flex flex-row justify-between an-3">
+    <div className="mt-3 flex flex-row justify-between items-center an-3">
       {/* 电脑端的显示设置 */}
-      <div className="mt-3 justify-self-start">
-        <Link className="p-2 text-lg font-bold text-black" to={"/"}>
+      <div className="">
+        <Link className="ml-2 text-lg font-bold text-black" to={"/"}>
           www.ventroar.xyz
         </Link>
       </div>
 
-      <div className="mr-7 font-bold text-slate-300 mt-3 hidden sm:block space-x-7">
+      <div className="mr-7 font-bold text-slate-300 hidden sm:block space-x-7">
         <Link className={ShowActive("/")} to={"/"}>
           <HomeIcon className="inline-block h-5 mb-1 w-5" />
           主页
@@ -45,7 +47,7 @@ const Nav = () => {
 
       {/* 手机端的显示设置 */}
       <Menu>
-        <Menu.Button className="absolute top-1 right-2 mr-2 mt-3 sm:hidden">
+        <Menu.Button className="mr-3 sm:hidden">
           <ViewListIcon className="h-5 w-5 text-black" />
         </Menu.Button>
 
@@ -58,8 +60,8 @@ const Nav = () => {
           className="an-2 font-bold text-slate-400 fixed flex flex-col bg-slate-100 
             items-center justify-evenly rounded-b-2xl h-[40vh] outline-none w-screen top-0 left-0 sm:hidden"
         >
-          <Menu.Button className="absolute top-1 right-2 mr-2 mt-3 sm:hidden">
-            <XIcon className="h-5 w-5 " />
+          <Menu.Button className="absolute top-1 right-1 mr-2 mt-3 sm:hidden">
+            <XIcon className="h-5 w-5" />
           </Menu.Button>
           <Menu.Item>
             <Link className={ShowActive("/")} to={"/"}>
