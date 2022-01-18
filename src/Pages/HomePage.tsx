ffb937b4ffc82bf695f3c97f520a828e6e328e80
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-18 13:40:20 +0800

    add:给home页面添加spring动画实验

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,10 +1,9 @@
 import axios from "axios";
 import { useState } from "react";
 import Footer from "../Components/Footer";
-import { Button } from "@mui/material";
+import { useSpring, animated } from "react-spring";
 const HomePage = () => {
   const [list, setList] = useState([]);
-
   async function getUser() {
     //利用异步方法请求数据
     const evenyUser = await axios("http://101.43.123.50:2546/userDataApi/");
@@ -12,10 +11,16 @@ const HomePage = () => {
   }
   let userCout = 0;
 
+  const [showtext, setShowtext] = useState(false);
+  const animation = useSpring({
+    opacity: showtext ? 1 : 0,
+    transform: showtext ? `translateY(0)` : `translateY(-200%)`,
+  });
+
   return (
     <>
       <div className="h-[93vh]">
-        <p>
+        <animated.p style={animation}>
           不用理会以下内容，还未完成...... reprehenderit
           <b className="text-sky-500">necessitatibus</b> aliquid! Sunt labore
           repellendus minus quas vel sint fugiat{" "}
@@ -26,14 +31,19 @@ const HomePage = () => {
             doloremque
           </b>
           nemo, ut facere, deserunt iure ab necessitatibus debitis!
-        </p>
+        </animated.p>
         <button
           className="button-style bg-green-400 text-white shadow-lg shadow-green-400/50"
           onClick={getUser}
         >
           Get Data
         </button>
-        <Button variant="contained">hello word</Button>
+        <button
+          className="button-style border-[1px] border-slate-200 shadow-lg"
+          onClick={() => setShowtext(!showtext)}
+        >
+          SHOW TEXT
+        </button>
       </div>
       {list.map((c: any) => (
         <ul key={c._id} className="overflow-x-auto">
