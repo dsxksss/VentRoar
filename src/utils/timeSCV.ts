52d1c86bfc4c5c4fe7bd1549b1fd4f90e6240718
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-21 16:03:56 +0800

    fix:时间戳转换函数类型错误过不了打包编译

--- src/utils/timeSCV.ts
+++ src/utils/timeSCV.ts
@@ -2,18 +2,24 @@
 //时间戳转换
 const timeSCV = (timeStamp: number) => {
   let time = new Date(timeStamp * 1000);
-  let year = time.getFullYear();
-  let month = time.getMonth() + 1;
-  let date = time.getDate();
-  let hours = time.getHours();
-  let minute = time.getMinutes();
-if (month < 10) { month = '0' + month }
-        if (date < 10) { date = '0' + date }
-        if (hours < 10) { hours = '0' + hours }
-        if (minute < 10) { minute = '0' + minute }
-    
-  return (
-    year + "-" + month + "-" + date + " " + hours + ":" + minute 
-  );
+  let year = time.getFullYear().toString();
+  let month = (time.getMonth() + 1).toString();
+  let date = time.getDate().toString();
+  let hours = time.getHours().toString();
+  let minute = time.getMinutes().toString();
+  if (parseInt(month) < 10) {
+    month = "0" + month;
+  }
+  if (parseInt(date) < 10) {
+    date = "0" + date;
+  }
+  if (parseInt(hours) < 10) {
+    hours = "0" + hours;
+  }
+  if (parseInt(minute) < 10) {
+    minute = "0" + minute;
+  }
+
+  return year + "-" + month + "-" + date + " " + hours + ":" + minute;
 };
 export default timeSCV;
