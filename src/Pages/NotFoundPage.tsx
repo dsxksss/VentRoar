717f09682893f4aa55fc9fe6a282fd6729a83e94
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-28 15:18:55 +0800

    add:404页面插画svg

--- src/Pages/NotFoundPage.tsx
+++ src/Pages/NotFoundPage.tsx
@@ -1,13 +1,17 @@
+import cimg from "../img/cImg/notFindPage.svg";
 function NotFoundPage() {
   return (
     <>
       <div className="h-[93vh] flex flex-col  justify-center items-center text-slate-900">
-        <div className="mt-[-7vh] text-center">
-          <h1 className="text-2xl ">404错误 没找到此网页!!!</h1>
+        <div className="bg-cover">
+          <img src={cimg} alt="notfindpage" />
+        </div>
+        <div className="text-center">
+          <h1 className="text-2xl ">错误! 没找到此网页!!!</h1>
           <h2 className="m-2 text-xl  mt-5 text-md">
             {`没找到此网页，请回到正确的网页路径!!!`}
           </h2>
-          <h1 className="mt-16 text-2xl ">404ERROR NOTFOUND!!!</h1>
+          <h1 className="mt-12 text-2xl ">ERROR! NOTFOUND!!!</h1>
           <h2 className="m-5 mt-5 text-lg ">
             {`Can’t find this webpage, please go back to the correct network path`}
           </h2>
