c860b1a22e54bc4337864373399233b713f02b7c
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-28 14:41:16 +0800

    fix:关于页面插画显示，文字等适配问题

--- src/Pages/AboutUsPage.tsx
+++ src/Pages/AboutUsPage.tsx
@@ -15,49 +15,53 @@ function HelpPage() {
       <div className="pt-[3rem] px-3 h-[93vh] overflow-y-auto">
         <div
           style={{ animationDuration: "2.5s" }}
-          className="an-3 overflow-auto"
+          className="an-3 overflow-auto flex flex-col-reverse sm:flex-row items-center justify-center "
         >
-          <img src={cimg} alt="developerCImg" />
-          <div className="shadow-xl">
-            <h1 className="text-[2.3rem] text-center mb-3">网站介绍</h1>
+          <div className="bg-cover">
+            <img src={cimg} alt="developerCImg" />
+          </div>
+          <div className="md:px-10">
+            <h1 className="text-[2.3rem] md:text-[2.7rem] lg:text-[3rem] text-center mb-3">
+              网站介绍
+            </h1>
 
-            <p className="text-[1rem] font-bold indent-4 text-center">
+            <p className="text-[1rem] md:text-[1.3rem] lg:text-[1.6rem] font-bold indent-4 text-center">
               倾诉最近遇到的不开心的事情，发泄到这上面，此网站用匿名的方式可以和别人交流倾诉最近遇到的不开心
             </p>
             <br />
-
             <p
               className="
-        text-center text-[1.2rem] font-bold text-red-600"
+        text-center text-[1.2rem] md:text-[1.5rem] lg:text-[1.8rem] font-bold text-red-600"
             >
               网站还在火速开发中🚀🚀🚀
               <br />
               版本V:0.2.5
             </p>
-
             <br />
-            <p>
-              待开发🚧 :
-              <br />
-              <b className="text-yellow-400">
-                网站主页样式、关于页面样式、Heart页面样式、Heart页面交互优化、用户主页样式及后台逻辑、优化页面交互、优化交互动画等、修复部分BUG......
-              </b>
-            </p>
-            <p>
-              已完成✅ :
-              <br />
-              <b className="text-green-500">
-                登入页面样式、注册页面样式、从数据库获取数据、登入和注册的后端逻辑、前后端数据库链接
-              </b>
-            </p>
           </div>
         </div>
-        <button
-          className="button-style bg-green-400 text-white shadow-lg shadow-green-400/50"
-          onClick={getUser}
-        >
-          Get Data
-        </button>
+        <div className="md:text-[1.7rem]">
+          <p>
+            待开发🚧 :
+            <br />
+            <b className="text-yellow-400">
+              网站主页样式、关于页面样式、Heart页面样式、Heart页面交互优化、用户主页样式及后台逻辑、优化页面交互、优化交互动画等、修复部分BUG......
+            </b>
+          </p>
+          <p>
+            已完成✅ :
+            <br />
+            <b className="text-green-500">
+              登入页面样式、注册页面样式、从数据库获取数据、登入和注册的后端逻辑、前后端数据库链接
+            </b>
+          </p>
+          <button
+            className="button-style bg-green-400 text-white shadow-lg shadow-green-400/50"
+            onClick={getUser}
+          >
+            Get Data
+          </button>
+        </div>
       </div>
       {list.map((c: any) => (
         <ul key={c._id} className="overflow-x-auto">
