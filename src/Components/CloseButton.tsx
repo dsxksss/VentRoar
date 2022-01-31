03923da935229677c8b614fa3c4f20f388bb023f
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-31 21:13:17 +0800

    add:浮入浮出动画

--- src/Components/CloseButton.tsx
+++ src/Components/CloseButton.tsx
@@ -1,6 +1,6 @@
 import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
 const CloseButton = () => (
-  <div className="flex justify-center items-center animate-[cloes_1.2s_ease-in-out_infinite]">
+  <div className="flex justify-center text-sm items-center animate-[cloes_1.5s_ease-in-out_infinite]">
     滑动关闭
     <ArrowNarrowRightIcon className="text-gray-900 w-7 h-5" />
   </div>
