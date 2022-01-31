c5e217a80c0dd55bf8e4244a15a52f216758ed07
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-02-01 01:09:43 +0800

    change:修改注释

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 ReactDOM.render(
+  //toast网络请求例子,参1:api请求,参2:设置三种状态显示的msg,参3:toast配置
+  // toast.promise(
+  //   new Promise((resolve) => setTimeout(resolve, 3000)),
+  //   {
+  //     pending: "发送中...",
+  //     success: "发送成功 👌",
+  //     error: "发送失败 网络繁忙 🤯",
+  //   },
+  //   {
+  //     autoClose: 1000,
+  //   }
+  // );
   <BrowserRouter>
     {/* 已y轴形势滑动关闭通知draggableDirection="y" */}
     <ToastContainer
