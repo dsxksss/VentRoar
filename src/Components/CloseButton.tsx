78a5122134004573661ad22d0ff11fcd0272adf5
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-02-04 20:34:45 +0800

    fix:修复在夜间模式下，消息提醒框箭头颜色

--- src/Components/CloseButton.tsx
+++ src/Components/CloseButton.tsx
@@ -2,7 +2,7 @@ import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
 const CloseButton = () => (
   <div className="flex justify-center text-sm items-center animate-[cloes_1.5s_ease-in-out_infinite]">
     滑动关闭
-    <ArrowNarrowRightIcon className="text-gray-900 w-7 h-5" />
+    <ArrowNarrowRightIcon className="dark:text-gray-100 text-gray-900 w-7 h-5" />
   </div>
 );
 export default CloseButton;
