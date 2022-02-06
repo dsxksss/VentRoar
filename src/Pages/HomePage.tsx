8a46e628bb91a8f894031bb84f246c8b4fe7046a
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-02-06 22:16:38 +0800

    change:优化主页样式(未完成)

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -11,6 +11,11 @@ const HomePage = () => {
             VentRoar
           </p>
         </div>
+        <div className="text-[1.1rem] dark:text-gray-100 font-bold px-4 py-1 space-y-2">
+          <p className="text-center">上天完全是为了坚强你的</p>
+          <p className="text-center">意志才在道路上设下重重的障碍</p>
+          <p className="text-right">———泰戈尔</p>
+        </div>
         <div className="flex justify-center items-center space-x-3 h-[30vh] w-[90vw] ">
           <div className="rounded-2xl shadow-md bg-slate-50 dark:bg-[#304053] duration-200 h-[28vh] transform ease-in-out ">
             <img
@@ -20,20 +25,20 @@ const HomePage = () => {
             />
           </div>
           <div className="flex font-bold flex-col space-y-5 justify-center">
-            <div className="flex indent-4 flex-col justify-center items-center h-[10vh] overflow-hidden duration-200 ease-in-out  bg-slate-50 dark:bg-[#304053] text-left rounded-2xl px-3 shadow-md py-4 text-green-500">
+            <div className="flex indent-4 flex-col justify-center font-bold items-center h-[10vh] overflow-hidden duration-200 ease-in-out  bg-slate-50 dark:bg-[#304053] text-left rounded-2xl px-3 shadow-md py-4 text-blue-500 dark:text-green-500">
               <span>不能因为一点小挫折就垂头丧气</span>
             </div>
-            <div className="flex flex-col justify-center items-center h-[15vh] overflow-hidden duration-200 ease-in-out  bg-slate-50 dark:bg-[#304053] rounded-2xl shadow-md px-3 py-4 text-center text-gray-900 dark:text-gray-300">
+            <div className="flex flex-col justify-center items-center font-bold h-[15vh] overflow-hidden duration-200 ease-in-out  bg-slate-50 dark:bg-[#304053] rounded-2xl shadow-md px-3 py-4 text-center text-gray-900 dark:text-gray-300">
               <span>再困难的事情都过去了不是吗?</span>
             </div>
           </div>
         </div>
         <div className="flex justify-center items-center space-x-3 h-[30vh] w-[90vw] ">
           <div className="flex font-bold flex-col space-y-5 justify-center">
-            <div className="flex flex-col justify-center items-center  h-[10vh] overflow-hidden duration-200 ease-in-out  bg-slate-50 dark:bg-[#304053] text-left rounded-2xl px-3 shadow-md py-4 text-green-500">
+            <div className="flex flex-col font-bold justify-center items-center  h-[10vh] overflow-hidden duration-200 ease-in-out  bg-slate-50 dark:bg-[#304053] text-left rounded-2xl px-3 shadow-md py-4 text-gray-900 dark:text-gray-300">
               <span>用户信息全加密</span>
             </div>
-            <div className="flex flex-col justify-center items-center h-[15vh] overflow-hidden duration-200 ease-in-out  bg-slate-50 dark:bg-[#304053] rounded-2xl shadow-md px-3 py-4 text-center text-gray-900 dark:text-gray-300">
+            <div className="flex flex-col font-bold justify-center items-center h-[15vh] overflow-hidden duration-200 ease-in-out  bg-slate-50 dark:bg-[#304053] rounded-2xl shadow-md px-3 py-4 text-center text-blue-500 dark:text-green-500">
               <span>以匿名的方式公布...</span>
             </div>
           </div>
