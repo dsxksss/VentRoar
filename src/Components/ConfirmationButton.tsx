302ca0bbace7396f2b78e66764f7a185218740b3
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-02-20 16:22:10 +0800

    fix:删除按钮组件出现异常的错误

--- src/Components/ConfirmationButton.tsx
+++ src/Components/ConfirmationButton.tsx
@@ -1,17 +1,19 @@
 const ConfirmationButton = (YES: any, NO: any, { closeToast }: any) => (
-  <div className="flex items-center justify-center space-x-3 text-sm">
-    <button
-      className="button-style rounded-full bg-red-500 text-gray-100 outline-none"
-      onClick={() => YES()}
-    >
-      <span className="font-bold">删除</span>
-    </button>
-    <button
-      className="button-style rounded-full bg-green-500 text-gray-100 outline-none"
-      onClick={() => (NO ? NO() : closeToast)}
-    >
-      <span className="font-bold">取消</span>
-    </button>
-  </div>
+  <>
+    <div className="flex items-center justify-center space-x-3 text-sm">
+      <button
+        className="button-style rounded-full bg-red-500 text-gray-100 outline-none"
+        onClick={() => YES()}
+      >
+        <span className="font-bold">删除</span>
+      </button>
+      <button
+        className="button-style rounded-full bg-green-500 text-gray-100 outline-none"
+        onClick={() => (NO ? NO() : closeToast)}
+      >
+        <span className="font-bold">取消</span>
+      </button>
+    </div>
+  </>
 );
 export default ConfirmationButton;
